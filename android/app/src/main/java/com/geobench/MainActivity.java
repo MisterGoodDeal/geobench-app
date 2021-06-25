7cef1650425685263ac78d8690c4642bdb4b027b
MisterGoodDeal <pro@turtletv.fr>
2021-06-25 23:46:21 +0200

    🚀 Project init

--- android/app/src/main/java/com/geobench/MainActivity.java
+++ android/app/src/main/java/com/geobench/MainActivity.java
@@ -1,4 +1,4 @@
-package com.boilerplate;
+package com.geobench;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "BoilerPlate";
+    return "Geobench";
   }
 }
