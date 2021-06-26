0c3c8e23c79f8b2aea856d8888fbe09b7c98a9c2
MisterGoodDeal <pro@turtletv.fr>
2021-06-27 00:41:49 +0200

    Update Colors.ts

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,7 +1,19 @@
 export const Colors = {
+  main: "#ffb300",
+  orangeGradient: {
+    start: "#ffd000",
+    middle: "#ffb300",
+    end: "#ff9900",
+  },
+  lightRed: "#ff4d4d",
+  transparent: "rgba(0,0,0,0)",
+  darkGrey: "#363636",
+  placeholder: "#878787",
+  inputColor: "#e9e9e9",
+  border: "#b0b0b0",
   white: "#fff",
   black: "#000",
-  background: "#fefefe",
+  background: "#f5f5f5",
   navigation: {
     focus: "#edab02",
     unfocus: "#bdbdbd",
