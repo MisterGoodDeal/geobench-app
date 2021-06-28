5583100ecd06f281cf4c7a46111685085cb1a331
MisterGoodDeal <pro@turtletv.fr>
2021-06-28 10:12:47 +0200

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,8 @@
+import { Platform } from "react-native";
+
 export const env = {
-  apiUrl: "http://192.168.125.156:3000",
+  apiUrl:
+    Platform.OS === "android"
+      ? "http://192.168.35.156:3000"
+      : "http://localhost:3000",
 };
