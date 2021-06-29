ac2b9793f4ce252bb6b0c01dc17fbb165d337008
MisterGoodDeal <pro@turtletv.fr>
2021-06-29 16:44:08 +0200

    feat: ➕ Implementing `Add bench` feature

---
+++ src/components/GBAddBench.tsx
@@ -0,0 +1,169 @@
+import * as React from "react";
+import {
+  StyleSheet,
+  Modal,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { Colors } from "../constants/Colors";
+import { Lang } from "../constants/Lang";
+import { Banc } from "../store/model/map";
+import { hp, openMap, wp } from "../utils/functions";
+import { GBButton } from "./GBButton";
+import { GBContainer } from "./GBContainer";
+import { GBImage } from "./GBImage";
+import { GBSpacer } from "./GBSpacer";
+import { GBText } from "./GBText";
+import { Rating } from "react-native-ratings";
+import { GBInput } from "./GBInput";
+import { GBRoundButton } from "./GBRoundButton";
+import { AddBench } from "../utils/interface";
+import { GBPicker, pickerSelectStyles } from "./GBPicker";
+import { launchCamera } from "react-native-image-picker";
+
+const BENCH_RATING = require("../assets/images/bench.png");
+
+interface GBAddBenchProps {
+  visible: boolean;
+  onClose: () => void;
+  addBench: () => void;
+  buttonUsable: boolean;
+  setNote: React.Dispatch<React.SetStateAction<number>>;
+  setLieu: React.Dispatch<React.SetStateAction<number>>;
+  setEnvironnement: React.Dispatch<React.SetStateAction<number>>;
+  setCommentaire: React.Dispatch<React.SetStateAction<string>>;
+  setPhoto: any;
+}
+
+export const GBAddBench: React.FunctionComponent<GBAddBenchProps> = ({
+  visible,
+  onClose,
+  addBench,
+  buttonUsable,
+  setNote,
+  setLieu,
+  setEnvironnement,
+  setCommentaire,
+  setPhoto,
+}) => (
+  <Modal animationType="slide" transparent={true} visible={visible}>
+    <View style={styles.centeredView}>
+      <View style={styles.modalView}>
+        <TouchableOpacity
+          onPress={onClose}
+          style={{
+            position: "absolute",
+            top: 10,
+            right: 10,
+          }}
+        >
+          <GBImage
+            size={"2.2%"}
+            source={require("../assets/images/close.png")}
+            tint={Colors.main}
+          />
+        </TouchableOpacity>
+
+        <GBText size={"4%"} style={"extra-bold"} align={"center"}>
+          {Lang.map.add.title}
+        </GBText>
+        <GBSpacer visible={false} space={"2%"} />
+        <Rating
+          type="star"
+          startingValue={0}
+          fractions={5}
+          imageSize={hp("4%")}
+          readonly={false}
+          onFinishRating={(rating: number) => setNote(rating)}
+        />
+        <GBText size={"1.4%"} style={"regular"} align={"center"}>
+          {Lang.map.add.rating}
+        </GBText>
+        <GBSpacer visible={false} space={"2%"} />
+        <GBPicker
+          placeholder={Lang.map.add.ph_location}
+          items={Lang.map.add.location}
+          setPickedItem={setLieu}
+        />
+        <GBSpacer visible={false} space={"2%"} />
+        <GBPicker
+          placeholder={Lang.map.add.ph_environment}
+          items={Lang.map.add.environment}
+          setPickedItem={setEnvironnement}
+        />
+        <GBSpacer visible={false} space={"2%"} />
+        <GBInput
+          multiline={false}
+          nbLines={1}
+          hook={setCommentaire}
+          type={"default"}
+          width={wp("69%")}
+          maxLength={250}
+          placeholder={Lang.map.add.comment}
+        />
+        <GBSpacer visible={false} space={"2%"} />
+        <GBButton
+          onPress={() =>
+            launchCamera(
+              {
+                saveToPhotos: false,
+                mediaType: "photo",
+                includeBase64: true,
+                quality: 0.5,
+              },
+              setPhoto
+            )
+          }
+          disable={false}
+        >
+          {Lang.map.add.photo}
+        </GBButton>
+        <GBSpacer visible={false} space={"4%"} />
+        <GBButton onPress={addBench} disable={!buttonUsable}>
+          {Lang.map.add.button}
+        </GBButton>
+      </View>
+    </View>
+  </Modal>
+);
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    width: "80%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.white,
+    borderRadius: 15,
+    padding: hp("3%"),
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+  },
+  modalTitle: {
+    textAlign: "center",
+    fontSize: hp("5%"),
+    color: Colors.darkGrey,
+    marginBottom: hp("5%"),
+  },
+  spinner: {
+    padding: hp("5%"),
+  },
+  spinnerOnly: {
+    position: "absolute",
+    top: 0,
+    alignItems: "center",
+  },
+});
--- src/screens/Carte.screen.tsx
+++ src/screens/Carte.screen.tsx
@@ -17,12 +17,12 @@ import { GBMapButton } from "../components/GBMapButton";
 import { api } from "../api";
 import { GBLoader } from "../components/GBLoader";
 import { mapSelector } from "../store/slices/mapSlice";
-import { GBImage } from "../components/GBImage";
-import { hp } from "../utils/functions";
 import { GBBenchDetails } from "../components/GBBenchDetails";
 import { Banc } from "../store/model/map";
 import { userSelector } from "../store/slices/userSlice";
 import { Lang } from "../constants/Lang";
+import { GBAddBench } from "../components/GBAddBench";
+import { GBToast } from "../components/GBToast";
 
 const marker = require("../assets/images/map/pin.png");
 
@@ -55,8 +55,28 @@ export const CarteScreen: React.FunctionComponent<null> = () => {
   const [loading, setLoading] = useState(false);
   const [usable, setUsable] = useState(false);
   const [comment, setComment] = useState("");
-  const { isFetching, isSuccess, isError, errorMessage, bancs } =
-    useSelector(mapSelector);
+
+  // Gestion de l'ajout d'un banc
+  const [addBench, setAddBench] = useState(false);
+  const [sendButton, setSendButton] = useState(false);
+  const [note, setNote] = useState(0);
+  const [lieu, setLieu] = useState(0);
+  const [environnement, setEnvironnement] = useState(0);
+  const [commentaire, setCommentaire] = useState("");
+  const [photo, setPhoto] = useState<any>(null);
+  const [photoUploaded, setPhotoUploaded] = useState(false);
+  const [benchUploaded, setBenchUploaded] = useState(false);
+  const [uploaded, setUploaded] = useState(false);
+
+  const {
+    isFetching,
+    isSuccess,
+    isError,
+    errorMessage,
+    bancs,
+    imageURL,
+    isUploaded,
+  } = useSelector(mapSelector);
   const { userInfo } = useSelector(userSelector);
   React.useEffect(() => {
     if (isFetching) {
@@ -66,48 +86,141 @@ export const CarteScreen: React.FunctionComponent<null> = () => {
     if (isSuccess) {
       // Handle success
       setLoading(false);
+      dispatch(actions.map.clearState());
       if (comment.length >= 10) {
-        Toast.show({
-          type: "success",
-          position: "top",
-          text1: Lang.map.commentSuccess.title,
-          text2: Lang.map.commentSuccess.text,
-          visibilityTime: 4000,
-          autoHide: true,
-          topOffset: 50,
-          bottomOffset: 40,
-          onShow: () => {},
-          onHide: () => {},
-          onPress: () => {},
-        });
+        GBToast(
+          Lang.map.commentSuccess.title,
+          Lang.map.commentSuccess.text,
+          "success"
+        );
         setComment("");
         dispatch(api.benches.get({}));
+        console.log("success after comment");
+      } else if (photoUploaded) {
+        uploadBench();
+        setPhotoUploaded(false);
+      } else if (isUploaded) {
+        console.log("uploaded");
+        GBToast(
+          Lang.map.add.upload.success.title,
+          Lang.map.add.upload.success.message,
+          "success"
+        );
+        resetAddBanc();
+        dispatch(api.benches.get({}));
+        setBenchUploaded(false);
       }
     }
     if (isError) {
       // Handle error
       setLoading(false);
-      setComment("");
-      console.log(errorMessage);
-      Toast.show({
-        type: "error",
-        position: "top",
-        text1: Lang.map.commentFail.title,
-        text2: Lang.map.commentFail.text,
-        visibilityTime: 4000,
-        autoHide: true,
-        topOffset: 50,
-        bottomOffset: 40,
-        onShow: () => {},
-        onHide: () => {},
-        onPress: () => {},
-      });
+      if (comment.length >= 10) {
+        setComment("");
+        GBToast(Lang.map.commentFail.title, Lang.map.commentFail.text, "error");
+      } else if (photoUploaded) {
+        GBToast(
+          Lang.map.add.photoToast.error.title,
+          Lang.map.add.photoToast.error.textUpload,
+          "error"
+        );
+        uploadBench();
+      } else if (isUploaded) {
+        setLoading(false);
+        GBToast(
+          Lang.map.add.upload.error.title,
+          Lang.map.add.upload.error.message,
+          "error"
+        );
+        setBenchUploaded(false);
+        resetAddBanc();
+      }
+      dispatch(actions.map.clearState());
     }
   }, [isFetching, isSuccess, isError, errorMessage]);
 
   React.useEffect(() => {
     getCurrentLocation();
   }, []);
+
+  React.useEffect(() => {
+    if (note === 0 || lieu === -1 || environnement === -1) {
+      setSendButton(false);
+    } else {
+      setSendButton(true);
+    }
+  }, [note, lieu, environnement, commentaire, photo]);
+
+  React.useEffect(() => {
+    if (
+      photo?.hasOwnProperty("errorCode") ||
+      photo?.hasOwnProperty("errorMessage")
+    ) {
+      Toast.show({
+        type: "error",
+        position: "top",
+        text1: Lang.map.add.photoToast.error.title,
+        text2: Lang.map.add.photoToast.error.text,
+        visibilityTime: 4000,
+        autoHide: true,
+        topOffset: 50,
+        bottomOffset: 40,
+        onShow: () => {},
+        onHide: () => {},
+        onPress: () => {
+          Toast.hide();
+        },
+      });
+    } else if (photo?.didCancel === true) {
+      Toast.show({
+        type: "info",
+        position: "top",
+        text1: Lang.map.add.photoToast.close.title,
+        text2: Lang.map.add.photoToast.close.text,
+        visibilityTime: 4000,
+        autoHide: true,
+        topOffset: 50,
+        bottomOffset: 40,
+        onShow: () => {},
+        onHide: () => {},
+        onPress: () => {
+          Toast.hide();
+        },
+      });
+    } else if (photo?.hasOwnProperty("assets")) {
+      Toast.show({
+        type: "success",
+        position: "top",
+        text1: Lang.map.add.photoToast.success.title,
+        text2: Lang.map.add.photoToast.success.text,
+        visibilityTime: 4000,
+        autoHide: true,
+        topOffset: 50,
+        bottomOffset: 40,
+        onShow: () => {},
+        onHide: () => {},
+        onPress: () => {
+          Toast.hide();
+        },
+      });
+    }
+  }, [photo]);
+
+  const resetAddBanc = () => {
+    dispatch(actions.map.clearState());
+    dispatch(actions.map.clearUpload());
+    dispatch(actions.map.clearImage());
+
+    setNote(0);
+    setLieu(-1);
+    setEnvironnement(-1);
+    setCommentaire("");
+    setPhoto(null);
+    setSendButton(false);
+    setPhotoUploaded(false);
+    setBenchUploaded(false);
+    setAddBench(false);
+    setUploaded(false);
+  };
 
   React.useEffect(() => {
     comment.length >= 10 && comment.length <= 250
@@ -126,10 +239,15 @@ export const CarteScreen: React.FunctionComponent<null> = () => {
         };
         setUserCoordinates(region);
       },
-      (error) => console.log(error),
+      (error) =>
+        GBToast(
+          Lang.map.position.error.title,
+          Lang.map.position.error.message,
+          "error"
+        ),
       {
         enableHighAccuracy: true,
-        timeout: 20000,
+        timeout: 5000,
         maximumAge: 1000,
       }
     );
@@ -143,6 +261,7 @@ export const CarteScreen: React.FunctionComponent<null> = () => {
         if (user !== "") {
           dispatch(actions.user.setUser(JSON.parse(user)));
           dispatch(api.benches.get({}));
+          console.log("success after useFocus");
           getCurrentLocation();
         } else {
           nav.navigate("Login");
@@ -154,6 +273,57 @@ export const CarteScreen: React.FunctionComponent<null> = () => {
       };
     }, [])
   );
+
+  const handleBenchSubmit = () => {
+    if (photo?.hasOwnProperty("assets")) {
+      const photoBase64 = photo.assets[0].base64;
+      setPhotoUploaded(true);
+      setLoading(true);
+      dispatch(api.benches.uploadImage({ image: photoBase64 }));
+    } else {
+      uploadBench();
+    }
+  };
+
+  const uploadBench = () => {
+    setBenchUploaded(true);
+    if (!uploaded) {
+      setUploaded(true);
+      Geolocation.getCurrentPosition(
+        async (position) => {
+          let region = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.0,
+          };
+          setUserCoordinates(region);
+          dispatch(
+            api.benches.post({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+              note: note,
+              lieu: `${lieu}`,
+              environnement: `${environnement}`,
+              commetaire: commentaire,
+              nom_photo: imageURL !== undefined ? imageURL : "",
+            })
+          );
+        },
+        (error) =>
+          GBToast(
+            Lang.map.position.bench_error.title,
+            Lang.map.position.bench_error.message,
+            "error"
+          ),
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 1000,
+        }
+      );
+    }
+  };
 
   return (
     <GBContainer
@@ -169,7 +339,7 @@ export const CarteScreen: React.FunctionComponent<null> = () => {
         showsScale={false}
         ref={mapRef}
         showsCompass={false}
-        initialRegion={parisRegion}
+        initialRegion={initialRegion}
         moveOnMarkerPress={true}
         showsPointsOfInterest={false}
         showsUserLocation={true}
@@ -220,7 +390,24 @@ export const CarteScreen: React.FunctionComponent<null> = () => {
           setComment("");
         }}
       />
-      <GBMapButton mv={mapRef} userCoordinates={userCoordinates} />
+
+      <GBAddBench
+        visible={addBench}
+        onClose={() => resetAddBanc()}
+        addBench={() => handleBenchSubmit()}
+        buttonUsable={sendButton}
+        setNote={setNote}
+        setLieu={setLieu}
+        setEnvironnement={setEnvironnement}
+        setCommentaire={setCommentaire}
+        setPhoto={setPhoto}
+      />
+
+      <GBMapButton
+        mv={mapRef}
+        userCoordinates={userCoordinates}
+        hookAddBench={setAddBench}
+      />
     </GBContainer>
   );
 };
