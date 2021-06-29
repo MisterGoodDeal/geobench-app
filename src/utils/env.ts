58116d1d7c93751724842b78c62acd939c9dd605
MisterGoodDeal <pro@turtletv.fr>
2021-06-29 16:40:03 +0200

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,6 +3,10 @@ import { Platform } from "react-native";
 export const env = {
   apiUrl:
     Platform.OS === "android"
-      ? "http://192.168.35.156:3000"
+      ? "http://192.168.82.156:3000"
       : "http://localhost:3000",
+  imgBB: {
+    key: "06faf38ae3e57ecce08a265d1168e417",
+    url: "https://api.imgbb.com",
+  },
 };
