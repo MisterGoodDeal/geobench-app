18cf4285102036e3c062db00bb047cd3f7086a6b
MisterGoodDeal <pro@turtletv.fr>
2021-07-04 00:01:18 +0200

    fix: 🖥 Switching to local env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,10 +1,7 @@
 import { Platform } from "react-native";
 
 export const env = {
-  apiUrl:
-    Platform.OS === "android"
-      ? "http://192.168.82.156:3000"
-      : "http://localhost:3000",
+  apiUrl: "http://localhost:3000",
   imgBB: {
     key: "06faf38ae3e57ecce08a265d1168e417",
     url: "https://api.imgbb.com",
