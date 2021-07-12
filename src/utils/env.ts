8fbae4ee80662a72b3d3e1b2edfc399edd20f544
MisterGoodDeal <pro@turtletv.fr>
2021-07-12 17:56:48 +0200

    feat: 💾 Changing API url

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,7 @@
 import { Platform } from "react-native";
 
 export const env = {
-  apiUrl: "http://87.89.222.228:3000",
+  apiUrl: "https://api.geobench.turtlecorp.fr",
   imgBB: {
     key: "06faf38ae3e57ecce08a265d1168e417",
     url: "https://api.imgbb.com",
