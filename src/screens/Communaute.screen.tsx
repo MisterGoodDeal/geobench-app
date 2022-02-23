9924f8ccc9a6adeab3c8e62abe4a579bba2a74b5
Milan Camus <pro@turtletv.fr>
2022-02-23 15:20:32 +0100

    feat: ✨ Add `ScrollView` for parameters and community screens

--- src/screens/Communaute.screen.tsx
+++ src/screens/Communaute.screen.tsx
@@ -1,7 +1,7 @@
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import * as React from "react";
 import { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import { Image } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { GBContainer } from "@components/GBContainer";
@@ -19,7 +19,12 @@ import { api } from "@api/index";
 import { GBToast } from "@components/GBToast";
 import { GBCard } from "@components/GBCard";
 import { Rating } from "react-native-ratings";
-import { FullscreenCaroussel, hp, Spacer } from "@mistergooddeal/rn-components";
+import {
+  FullscreenCaroussel,
+  hp,
+  Spacer,
+  wp,
+} from "@mistergooddeal/rn-components";
 import { GBClose } from "@components/GBClose";
 
 export const CommunauteScreen: React.FunctionComponent<null> = () => {
@@ -202,49 +207,29 @@ export const CommunauteScreen: React.FunctionComponent<null> = () => {
             color={darkMode ? ColorsDark.background : Colors.background}
             extraStyle={{ borderTopLeftRadius: 30, borderTopRightRadius: 30 }}
           >
-            <GBText
-              style={"black"}
-              size={"3.5%"}
-              align={"center"}
-              color={darkMode ? ColorsDark.white : undefined}
+            <ScrollView
+              contentContainerStyle={{
+                alignItems: "center",
+                height: "100%",
+                width: wp("100%"),
+              }}
+              showsVerticalScrollIndicator={false}
             >
-              {Lang.community.your_stat}
-            </GBText>
-            <GBContainer
-              direction={"row"}
-              justifyContent={"space-evenly"}
-              extraStyle={{ width: "100%" }}
-            >
-              <GBCard
-                width={"40%"}
-                color={darkMode ? ColorsDark.inputColor : undefined}
+              <Spacer space={"5%"} visible={false} />
+              <GBText
+                style={"black"}
+                size={"3.5%"}
+                align={"center"}
+                color={darkMode ? ColorsDark.white : undefined}
               >
-                <GBContainer alignItems={"center"} justifyContent={"center"}>
-                  <GBText
-                    style={"black"}
-                    size={"3%"}
-                    align={"center"}
-                    color={darkMode ? ColorsDark.white : undefined}
-                  >
-                    {community !== null
-                      ? community?.user.benchCount.toString()
-                      : "-"}
-                  </GBText>
-                  <GBImage
-                    source={require("../assets/images/bench.png")}
-                    size={"5%"}
-                  />
-                  <GBText
-                    style={"regular"}
-                    size={"1.5%"}
-                    align={"center"}
-                    color={darkMode ? ColorsDark.white : undefined}
-                  >
-                    {Lang.community.bench.user}
-                  </GBText>
-                </GBContainer>
-              </GBCard>
-              <TouchableOpacity onPress={() => setUserPhotosVisible(true)}>
+                {Lang.community.your_stat}
+              </GBText>
+              <Spacer space={"1.5%"} visible={false} />
+              <GBContainer
+                direction={"row"}
+                justifyContent={"space-evenly"}
+                extraStyle={{ width: "100%" }}
+              >
                 <GBCard
                   width={"40%"}
                   color={darkMode ? ColorsDark.inputColor : undefined}
@@ -257,11 +242,11 @@ export const CommunauteScreen: React.FunctionComponent<null> = () => {
                       color={darkMode ? ColorsDark.white : undefined}
                     >
                       {community !== null
-                        ? community?.user.photosCount.toString()
+                        ? community?.user.benchCount.toString()
                         : "-"}
                     </GBText>
                     <GBImage
-                      source={require("../assets/images/camera.png")}
+                      source={require("../assets/images/bench.png")}
                       size={"5%"}
                     />
                     <GBText
@@ -270,79 +255,83 @@ export const CommunauteScreen: React.FunctionComponent<null> = () => {
                       align={"center"}
                       color={darkMode ? ColorsDark.white : undefined}
                     >
-                      {Lang.community.photo.user}
+                      {Lang.community.bench.user}
                     </GBText>
                   </GBContainer>
                 </GBCard>
-              </TouchableOpacity>
-            </GBContainer>
-            <Spacer visible={false} space={"2%"} />
-            <GBCard
-              width={"85%"}
-              color={darkMode ? ColorsDark.inputColor : undefined}
-            >
+                <TouchableOpacity onPress={() => setUserPhotosVisible(true)}>
+                  <GBCard
+                    width={"40%"}
+                    color={darkMode ? ColorsDark.inputColor : undefined}
+                  >
+                    <GBContainer
+                      alignItems={"center"}
+                      justifyContent={"center"}
+                    >
+                      <GBText
+                        style={"black"}
+                        size={"3%"}
+                        align={"center"}
+                        color={darkMode ? ColorsDark.white : undefined}
+                      >
+                        {community !== null
+                          ? community?.user.photosCount.toString()
+                          : "-"}
+                      </GBText>
+                      <GBImage
+                        source={require("../assets/images/camera.png")}
+                        size={"5%"}
+                      />
+                      <GBText
+                        style={"regular"}
+                        size={"1.5%"}
+                        align={"center"}
+                        color={darkMode ? ColorsDark.white : undefined}
+                      >
+                        {Lang.community.photo.user}
+                      </GBText>
+                    </GBContainer>
+                  </GBCard>
+                </TouchableOpacity>
+              </GBContainer>
+              <Spacer visible={false} space={"2%"} />
+              <GBCard
+                width={"85%"}
+                color={darkMode ? ColorsDark.inputColor : undefined}
+              >
+                <GBText
+                  style={"regular"}
+                  size={"1.5%"}
+                  align={"center"}
+                  color={darkMode ? ColorsDark.white : undefined}
+                >
+                  {`${Lang.community.rating} ${community?.user.avgBench.text}`}
+                </GBText>
+                <Spacer visible={false} space={".5%"} />
+                <Rating
+                  type="star"
+                  startingValue={community?.user.avgBench.full}
+                  fractions={5}
+                  imageSize={hp("4%")}
+                  readonly={true}
+                  tintColor={darkMode ? ColorsDark.inputColor : Colors.white}
+                />
+              </GBCard>
+              <Spacer visible={false} space={"3%"} />
               <GBText
-                style={"regular"}
-                size={"1.5%"}
+                style={"black"}
+                size={"3.5%"}
                 align={"center"}
                 color={darkMode ? ColorsDark.white : undefined}
               >
-                {`${Lang.community.rating} ${community?.user.avgBench.text}`}
+                {Lang.community.community}
               </GBText>
-              <Spacer visible={false} space={".5%"} />
-              <Rating
-                type="star"
-                startingValue={community?.user.avgBench.full}
-                fractions={5}
-                imageSize={hp("4%")}
-                readonly={true}
-                tintColor={darkMode ? ColorsDark.inputColor : Colors.white}
-              />
-            </GBCard>
-            <Spacer visible={false} space={"5%"} />
-            <GBText
-              style={"black"}
-              size={"3.5%"}
-              align={"center"}
-              color={darkMode ? ColorsDark.white : undefined}
-            >
-              {Lang.community.community}
-            </GBText>
-            <GBContainer
-              direction={"row"}
-              justifyContent={"space-evenly"}
-              extraStyle={{ width: "100%" }}
-            >
-              <GBCard
-                width={"30%"}
-                color={darkMode ? ColorsDark.inputColor : undefined}
+              <Spacer space={"1.5%"} visible={false} />
+              <GBContainer
+                direction={"row"}
+                justifyContent={"space-evenly"}
+                extraStyle={{ width: "100%" }}
               >
-                <GBContainer alignItems={"center"} justifyContent={"center"}>
-                  <GBText
-                    style={"black"}
-                    size={"3%"}
-                    align={"center"}
-                    color={darkMode ? ColorsDark.white : undefined}
-                  >
-                    {community !== null
-                      ? community?.global.benchCount.toString()
-                      : "-"}
-                  </GBText>
-                  <GBImage
-                    source={require("../assets/images/bench.png")}
-                    size={"5%"}
-                  />
-                  <GBText
-                    style={"regular"}
-                    size={"1.5%"}
-                    align={"center"}
-                    color={darkMode ? ColorsDark.white : undefined}
-                  >
-                    {Lang.community.bench.community}
-                  </GBText>
-                </GBContainer>
-              </GBCard>
-              <TouchableOpacity onPress={() => setCommunityPhotoVisible(true)}>
                 <GBCard
                   width={"30%"}
                   color={darkMode ? ColorsDark.inputColor : undefined}
@@ -355,11 +344,11 @@ export const CommunauteScreen: React.FunctionComponent<null> = () => {
                       color={darkMode ? ColorsDark.white : undefined}
                     >
                       {community !== null
-                        ? community.global.photosCount.toString()
+                        ? community?.global.benchCount.toString()
                         : "-"}
                     </GBText>
                     <GBImage
-                      source={require("../assets/images/camera.png")}
+                      source={require("../assets/images/bench.png")}
                       size={"5%"}
                     />
                     <GBText
@@ -368,41 +357,78 @@ export const CommunauteScreen: React.FunctionComponent<null> = () => {
                       align={"center"}
                       color={darkMode ? ColorsDark.white : undefined}
                     >
-                      {Lang.community.photo.community}
+                      {Lang.community.bench.community}
                     </GBText>
                   </GBContainer>
                 </GBCard>
-              </TouchableOpacity>
-              <GBCard
-                width={"30%"}
-                color={darkMode ? ColorsDark.inputColor : undefined}
-              >
-                <GBContainer alignItems={"center"} justifyContent={"center"}>
-                  <GBText
-                    style={"black"}
-                    size={"3%"}
-                    align={"center"}
-                    color={darkMode ? ColorsDark.white : undefined}
+                <TouchableOpacity
+                  onPress={() => setCommunityPhotoVisible(true)}
+                >
+                  <GBCard
+                    width={"30%"}
+                    color={darkMode ? ColorsDark.inputColor : undefined}
                   >
-                    {community !== null
-                      ? community.global.usersCount.toString()
-                      : "-"}
-                  </GBText>
-                  <GBImage
-                    source={require("../assets/images/users.png")}
-                    size={"5%"}
-                  />
-                  <GBText
-                    style={"regular"}
-                    size={"1.5%"}
-                    align={"center"}
-                    color={darkMode ? ColorsDark.white : undefined}
-                  >
-                    {Lang.community.user}
-                  </GBText>
-                </GBContainer>
-              </GBCard>
-            </GBContainer>
+                    <GBContainer
+                      alignItems={"center"}
+                      justifyContent={"center"}
+                    >
+                      <GBText
+                        style={"black"}
+                        size={"3%"}
+                        align={"center"}
+                        color={darkMode ? ColorsDark.white : undefined}
+                      >
+                        {community !== null
+                          ? community.global.photosCount.toString()
+                          : "-"}
+                      </GBText>
+                      <GBImage
+                        source={require("../assets/images/camera.png")}
+                        size={"5%"}
+                      />
+                      <GBText
+                        style={"regular"}
+                        size={"1.5%"}
+                        align={"center"}
+                        color={darkMode ? ColorsDark.white : undefined}
+                      >
+                        {Lang.community.photo.community}
+                      </GBText>
+                    </GBContainer>
+                  </GBCard>
+                </TouchableOpacity>
+                <GBCard
+                  width={"30%"}
+                  color={darkMode ? ColorsDark.inputColor : undefined}
+                >
+                  <GBContainer alignItems={"center"} justifyContent={"center"}>
+                    <GBText
+                      style={"black"}
+                      size={"3%"}
+                      align={"center"}
+                      color={darkMode ? ColorsDark.white : undefined}
+                    >
+                      {community !== null
+                        ? community.global.usersCount.toString()
+                        : "-"}
+                    </GBText>
+                    <GBImage
+                      source={require("../assets/images/users.png")}
+                      size={"5%"}
+                    />
+                    <GBText
+                      style={"regular"}
+                      size={"1.5%"}
+                      align={"center"}
+                      color={darkMode ? ColorsDark.white : undefined}
+                    >
+                      {Lang.community.user}
+                    </GBText>
+                  </GBContainer>
+                </GBCard>
+              </GBContainer>
+              <Spacer space={"5%"} visible={false} />
+            </ScrollView>
           </GBContainer>
         </GBContainer>
       )}
